2b64040b9d6c83710da0709cab3d729ba18b6a23
Angela del Pan <angeladelpan@gmail.com>
2025-04-09 16:01:29 +0200

    nuevo entreno, falta postman

---
+++ server/src/controllers/trainings/createTrainingController.js
@@ -0,0 +1,50 @@
+//importamos modelos
+import createTrainingModel from '../../models/trainings/createTrainingModel.js';
+import getUserByIdModel from '../../models/users/getUserByIdModel.js';
+
+//importamos función que lanza errores
+import generateErrorUtil from '../../utils/generateErrorUtil.js';
+
+//función controladora que crea un entrenamiento
+const createTrainingController = async (req, res, next) => {
+    try {
+        //obtenemos los campos necesarios del body
+        const { title, description } = req.body;
+
+        //si faltan campos lanzamos un error
+        if (!title || !description) {
+            generateErrorUtil('Faltan campos', 400);
+        }
+
+        //obtenemos el id del coach
+        const coachId = await getUserByIdModel(req.user.id);
+
+        //mensaje si somos users
+        if (user.role === 'user') {
+            generateErrorUtil(
+                'No tienes permisos para crear un entrenamiento',
+                401,
+            );
+        }
+
+        //insertamos el entrenamiento en la base de datos
+        const trainingId = await createTrainingModel(
+            title,
+            description,
+            coachId,
+        );
+
+        //enviamos la respuesta
+        res.send({
+            status: 'ok',
+            message: 'Entrenamiento creado con éxito',
+            data: {
+                trainingId,
+            },
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+//exportamos la función
+export default createTrainingController;
